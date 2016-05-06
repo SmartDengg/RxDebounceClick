529d1cebd8129fc36478bba9271a94c0849aceb5
SmartDengg <dengwei@lianjia.com>
2016-05-06 17:32:24 +0800

    增加时区限定

--- app/src/main/java/com/smartdengg/example/Entity.java
+++ app/src/main/java/com/smartdengg/example/Entity.java
@@ -2,13 +2,15 @@ package com.smartdengg.example;
 
 import android.annotation.SuppressLint;
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * Created by SmartDengg on 2016/4/30.
  */
 public class Entity {
 
-    @SuppressLint("SimpleDateFormat") private SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss·S");
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss·S", Locale.CHINA);
 
     private long timeline;
     private String processInfo;
@@ -20,6 +22,7 @@ public class Entity {
 
     @Override
     public String toString() {
-        return "Trigger ⇢\n" + "    Timeline = '" + formatter.format(timeline) + "'\n    ProcessInfo = '" + processInfo + "'";
+        return "Trigger ⇢\n" + "    Timeline = '" + formatter.format(timeline) +
+                "'\n    ProcessInfo = '" + processInfo + "'";
     }
 }
